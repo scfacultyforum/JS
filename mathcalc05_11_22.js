6c59e2ba10eacd77fb528c5ad3ad1f8c86b0d6b3
scfacultyforum <62398333+scfacultyforum@users.noreply.github.com>
2022-05-11 17:02:09 -0400

    Update mathcalc05_11_22.js

--- mathcalc05_11_22.js
+++ mathcalc05_11_22.js
@@ -4,9 +4,9 @@ function totalGPA()
 {
   var theForm = document.forms["weightedmath"];
 	var hs = theForm.elements["hsgpa"].value;
-	var alg = theForm.elements["alg1"];
+	var alg = theForm.elements["alg1"].value;
 	var geom = theForm.elements["geo"].value;
- 	var algii = theForm.elements["alg2"];
+ 	var algii = theForm.elements["alg2"].value;
 	var recent = theForm.elements["last"].value;
 	
     var total = 0.1*hs+0.15*alg+0.3*geom+0.3*algii+0.15*recent;
