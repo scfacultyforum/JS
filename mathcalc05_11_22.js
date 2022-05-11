f2ef96e229df6309bd4f4574fcbef8e29885d6eb
scfacultyforum <62398333+scfacultyforum@users.noreply.github.com>
2022-05-11 16:58:34 -0400

    Create mathcalc05_11_22.js

---
+++ mathcalc05_11_22.js
@@ -0,0 +1,19 @@
+var theForm = document.forms["weightedmath"];
+
+function totalGPA()
+{
+  var theForm = document.forms["weightedmath"];
+	var hs = theForm.elements["hsgpa"].value;
+	var alg = theForm.elements["alg1"];
+	var geom = theForm.elements["geo"].value;
+ 	var algii = theForm.elements["alg2"];
+	var recent = theForm.elements["last"].value;
+	
+    var total = 0.1*hs+0.15*alg+0.3*geom+0.3*algii+0.15*recent;
+
+    document.getElementById('totalWages').innerHTML =
+                                      "Math Weighted GPA: "+total; 
+   					  
+
+ 
+}
