341029f333850604c81095525f0d98c0f3cec2e1
scfacultyforum <62398333+scfacultyforum@users.noreply.github.com>
2024-03-15 12:48:52 -0400

    Update and rename mathcalc031424v10.js to mathcalc031424v11.js

--- mathcalc031424v11.js
+++ mathcalc031424v11.js
@@ -9,12 +9,12 @@ function calculateGPA()
  	var algii = theForm.elements["alg2"].value;
 	var recent = theForm.elements["last"].value;
   var levelSelect = theForm.elements["level"];
-	if(levelSelect[0].checked){var total = 0.1*hs+0.15*alg+0.15*geom+0.3*algii+0.3*recent;}
-	if(levelSelect[1].checked){var total = 0.1*hs+0.15*alg+0.3*geom+0.3*algii+0.15*recent;}
+	if(levelSelect[0].checked){var total = (0.1*hs+0.15*alg+0.15*geom+0.3*algii+0.3*recent).toFixed(1);}
+	if(levelSelect[1].checked){var total = (0.1*hs+0.15*alg+0.3*geom+0.3*algii+0.15*recent).toFixed(1);}
 	if(levelSelect[2].checked){var total = 0;}
 if(total >= 3){var recommend = 'We would recommend you can take a college level course (Math 122)';}
-if(total >= 2 && total <3){var recommend='We would recommend you can take a corequisite support course (Math 022)';}
-if(total<2){var recommend='We would recommend you receive additional help before attempting a math course.';}
+if(total >= 2 && total < 3){var recommend='We would recommend you can take a corequisite support course (Math 022)';}
+if(total < 2){var recommend='We would recommend you receive additional help before attempting a math course.';}
 
 document.getElementById('totalGPA').innerHTML = "Recommendation: "+recommend;
  }                                
